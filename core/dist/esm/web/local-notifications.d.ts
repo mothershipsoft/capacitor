da70e1ab6c66d76b983cb748d930cf0131f690e5
Andy Cork <andy.cork@gmail.com>
2020-03-23 15:17:26 +0000

    Build of electron and core

---
+++ core/dist/esm/web/local-notifications.d.ts
@@ -0,0 +1,23 @@
+import { WebPlugin } from './index';
+import { LocalNotificationsPlugin, LocalNotificationEnabledResult, LocalNotificationPendingList, LocalNotificationActionType, LocalNotification, LocalNotificationScheduleResult, NotificationPermissionResponse } from '../core-plugin-definitions';
+import { PermissionsRequestResult } from '../definitions';
+export declare class LocalNotificationsPluginWeb extends WebPlugin implements LocalNotificationsPlugin {
+    private pending;
+    constructor();
+    sendPending(): void;
+    sendNotification(localNotification: LocalNotification): Notification;
+    buildNotification(localNotification: LocalNotification): Notification;
+    schedule(options: {
+        notifications: LocalNotification[];
+    }): Promise<LocalNotificationScheduleResult>;
+    getPending(): Promise<LocalNotificationPendingList>;
+    registerActionTypes(_options: {
+        types: LocalNotificationActionType[];
+    }): Promise<void>;
+    cancel(pending: LocalNotificationPendingList): Promise<void>;
+    areEnabled(): Promise<LocalNotificationEnabledResult>;
+    requestPermission(): Promise<NotificationPermissionResponse>;
+    requestPermissions(): Promise<PermissionsRequestResult>;
+}
+declare const LocalNotifications: LocalNotificationsPluginWeb;
+export { LocalNotifications };
